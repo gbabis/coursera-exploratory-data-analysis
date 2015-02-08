aee79c64eb6371211254b7d7fe98c6d33af79935
gbabis <babisgrouzakis@gmail.com>
2015-02-08 21:21:41 +0100

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,19 +1,19 @@
-## Read the full dataset
+## Read the full dataset and replace NAs with "?"
 data <- read.csv("./data/household_power_consumption.txt", sep=';', na.strings="?")
 
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
-## Subsetting the data
-data_sub <- subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+## Subsetting the data according to the specified time frame
+data_sub <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
 rm(data)
 
-## Converting dates
-datetime <- paste(as.Date(data_sub$Date), data_sub$Time)
+## Converting to datetime using POSIXct
+datetime <- with(data_sub, paste(Date, Time))
 data_sub$DateTime <- as.POSIXct(datetime)
 
 ## Plot 2
-plot(data_sub$Global_active_power~data_sub$DateTime, type ="l", xlab="",
-     ylab="Global Active Power (kilowatts)")
+with(data_sub, plot(Global_active_power ~ DateTime, type = "l", xlab = "",
+                    ylab ="Global Active Power (kilowatts"))
 
 ## Saving to file
 dev.copy(png, file="plot2.png", height=480, width=480)
